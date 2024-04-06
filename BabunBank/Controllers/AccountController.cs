a58abdcb0ecd4a8183142ca127b6577b9db9355c
kahol <kahola@gmail.com>
2024-04-06 16:14:23 +0200

    added some questiond for richard

--- BabunBank/Controllers/AccountController.cs
+++ BabunBank/Controllers/AccountController.cs
@@ -1,15 +1,18 @@
-﻿using BabunBank.Models.Account;
+﻿using BabunBank.Configurations;
+using BabunBank.Models.Account;
 using BabunBank.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BabunBank.Controllers;
 
+[Authorize(Roles = $"{RoleNames.Admin}, {RoleNames.Cashier}")]
 public class AccountController(AccountService accountService) : Controller
 {
     public async Task<IActionResult> Details(int id)
     {
         var result = await accountService.GetAccountViewModelAsync(id);
-        
+
         return View(result);
     }
 }
