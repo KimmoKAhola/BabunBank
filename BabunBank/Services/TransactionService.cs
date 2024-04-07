1971e8d05eaf8b29d23d239d2ffdcc06e6d2d688
kahol <kahola@gmail.com>
2024-04-07 17:12:20 +0200

    TransactionService.cs was created. Only GetTransaction (not correctly implemented) and CreateTransaction are implemented for now

---
+++ BabunBank/Services/TransactionService.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLibrary.Data;
+using DataAccessLibrary.DataServices;
+
+namespace BabunBank.Services;
+
+public class TransactionService(DataTransactionService dataTransactionService)
+{
+    public async Task<Transaction> GetTransaction()
+    {
+        return await dataTransactionService.GetAsync(960);
+    }
+
+    public async Task<bool?> CreateTransactionAsync(Transaction transaction)
+    {
+        return await dataTransactionService.CreateAsync(transaction);
+    }
+}
