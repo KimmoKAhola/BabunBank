378bca49a9e83cce17679bad7e0cc9d757d4177f
kahol <kahola@gmail.com>
2024-04-08 20:13:35 +0200

    Created a CreateTransferModel.cs, not used yet

--- BabunBank/Models/Cashier/CreateTransferModel.cs
+++ BabunBank/Models/Cashier/CreateTransferModel.cs
@@ -1,6 +1,31 @@
-﻿namespace BabunBank.Models.Cashier;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabunBank.Models.Cashier;
 
 public class CreateTransferModel
 {
-    
+    [Required]
+    public int FromAccountId { get; init; }
+
+    [Required]
+    public int ToAccountId { get; init; }
+
+    [Required]
+    public DateOnly Date { get; init; }
+
+    [Required]
+    public string Type { get; init; } = null!;
+
+    [Required]
+    public string Operation { get; init; } = null!;
+
+    [Required]
+    [Range(100, 10000)]
+    public decimal Amount { get; init; }
+
+    [Required]
+    public decimal Balance { get; init; }
+    public string? Symbol { get; init; }
+    public string? Bank { get; init; }
+    public string? Account { get; init; }
 }
