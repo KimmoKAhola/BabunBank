58d6cca547ff06914d566b2b67bd81e5aa76f191
kahol <kahola@gmail.com>
2024-04-08 10:12:20 +0200

    My TransactionRepository.cs overrides the BaseRepository class. I have special logic for transactions

--- DataAccessLibrary/Repositories/TransactionRepository.cs
+++ DataAccessLibrary/Repositories/TransactionRepository.cs
@@ -1,6 +1,24 @@
 ﻿using DataAccessLibrary.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLibrary.Repositories;
 
 public class TransactionRepository(BankAppDataContext dbContext)
-    : BaseRepository<Transaction>(dbContext) { }
+    : BaseRepository<Transaction>(dbContext)
+{
+    public override async Task<Transaction?> CreateAsync(Transaction entity)
+    {
+        try
+        {
+            entity.Balance += entity.Amount; //Should be sum of transactions
+            dbContext.Set<Transaction>().Add(entity);
+            await dbContext.SaveChangesAsync();
+            return entity;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return null!;
+        }
+    }
+}
