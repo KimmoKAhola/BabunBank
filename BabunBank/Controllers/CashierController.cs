5a9491f6ade7f1c7fdd456546b59f8eb0f21a548
kahol <kahola@gmail.com>
2024-04-08 20:11:50 +0200

    Removed the NotFound from CashierController.cs. Replaced it with RedirectToAction instead

--- BabunBank/Controllers/CashierController.cs
+++ BabunBank/Controllers/CashierController.cs
@@ -91,7 +91,6 @@ public class CashierController(CustomerService customerService, IMapper mapper) 
         else
         {
             TempData["ErrorMessage"] = "A CATASTROPHIC ERROR OCCURED!";
-            return NotFound();
             return RedirectToAction("Index", "Error");
         }
 
