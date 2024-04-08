d54e4f105fbf3b8c18fc34607e9bf8584c6d289c
kahol <kahola@gmail.com>
2024-04-08 20:15:31 +0200

    added Methods to my TransactionFactory.cs. These will be used when creating transactions to/from accounts

---
+++ BabunBank/Factories/TransactionFactory.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using BabunBank.Models.Account;
+using BabunBank.Models.Cashier;
+using DataAccessLibrary.Data;
+
+namespace BabunBank.Factories;
+
+public static class TransactionFactory
+{
+    public static Transaction CreateDeposit(CreateDepositModel model)
+    {
+        try
+        {
+            var transaction = new Transaction
+            {
+                AccountId = model.AccountId,
+                Date = model.Date,
+                Type = model.Type,
+                Operation = model.Operation,
+                Amount = model.Amount,
+                Balance = model.Balance,
+                Symbol = model.Symbol,
+                Bank = model.Bank,
+                Account = model.Account
+            };
+
+            return transaction;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public static Transaction CreateWithdrawal(CreateWithdrawalModel model)
+    {
+        try
+        {
+            var transaction = new Transaction
+            {
+                AccountId = model.AccountId,
+                Date = model.Date,
+                Type = model.Type,
+                Operation = model.Operation,
+                Amount = (model.Amount * -1),
+                Balance = model.Balance,
+                Symbol = model.Symbol,
+                Bank = model.Bank,
+                Account = model.Account
+            };
+
+            return transaction;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
