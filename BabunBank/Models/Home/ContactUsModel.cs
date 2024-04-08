a4ef95b8c55d4e9ccf57d39604717a5344a21e5e
kahol <kahola@gmail.com>
2024-04-08 20:12:17 +0200

    Created a model for the contact page, ContactUsModel.cs

---
+++ BabunBank/Models/Home/ContactUsModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Build.Framework;
+
+namespace BabunBank.Models.Home;
+
+public class ContactUsModel
+{
+    [Required]
+    public string FirstName { get; init; } = null!;
+
+    [Required]
+    public string LastName { get; init; } = null!;
+
+    [Required]
+    public string Email { get; init; } = null!;
+
+    [Required]
+    public string Message { get; init; } = null!;
+}
