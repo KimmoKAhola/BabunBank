e49ebc20dd41de123be04774a89cb709096282c0
kahol <kahola@gmail.com>
2024-04-08 10:11:17 +0200

    My CreateAsync in the DataTransactionService.cs is now done. The Deposit method can most likely be removed since this handles all the logic

--- DataAccessLibrary/DataServices/DataTransactionService.cs
+++ DataAccessLibrary/DataServices/DataTransactionService.cs
@@ -1,10 +1,13 @@
 ﻿using DataAccessLibrary.Data;
 using DataAccessLibrary.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLibrary.DataServices;
 
-public class DataTransactionService(TransactionRepository transactionRepository)
-    : IDataService<Transaction>
+public class DataTransactionService(
+    TransactionRepository transactionRepository,
+    AccountRepository accountRepository
+) : IDataService<Transaction>
 {
     public Transaction Deposit(int id, decimal amount, Account account)
     {
@@ -20,44 +23,37 @@ public class DataTransactionService(TransactionRepository transactionRepository)
 
     public async Task<bool?> CreateAsync(Transaction model)
     {
-        await transactionRepository.CreateAsync(model);
+        //Get the account associated with the model
+        var account = await accountRepository.GetAsync(x => x.AccountId == model.AccountId);
+        if (account == null)
+            return null!;
+
+        //Create a new transaction
+        var newTransaction = await transactionRepository.CreateAsync(model);
+
+        //Sum of all transaction amounts
+        var newAccountBalance = await transactionRepository
+            .GetAllAsync()
+            .Where(x => x.AccountId == newTransaction.AccountId)
+            .SumAsync(x => x.Amount);
+
+        //Update the balance according to transactions
+        account.Balance = newAccountBalance;
+
+        //Update the account. Check for null here? TODO
+        await accountRepository.UpdateAsync(x => x.AccountId == account.AccountId, account);
         return true;
     }
 
     public async Task<Transaction?> Transfer()
     {
-        // var fromAccount = await dataAccountService.GetAsync(fromAccId);
-        // var toAccount = await dataAccountService.GetAsync(toAccId);
-        //
-        // if (fromAccount == null || toAccount == null)
-        //     return false;
-        // if (amount > fromAccount.Balance)
-        //     return false;
-        //
-        // fromAccount.Balance -= amount;
-        // toAccount.Balance += amount;
-        //
-        // await dataAccountService.UpdateAsync(fromAccount);
-        // await dataAccountService.UpdateAsync(toAccount);
-        // return true;
-
+        //TODO This can use deposit and withdraw since they have the logic already
         return null!;
     }
 
     public async Task<Transaction?> Withdraw()
     {
-        // var account = await dataAccountService.GetAsync(id);
-        //
-        // if (account == null)
-        //     return false;
-        // if (amount > account.Balance)
-        // {
-        //     return false;
-        // }
-        //
-        // account.Balance -= amount;
-        // await dataAccountService.UpdateAsync(account);
-        // return true;
+        //TODO check the deposit
 
         return null!;
     }
@@ -70,6 +66,11 @@ public class DataTransactionService(TransactionRepository transactionRepository)
     public IQueryable<Transaction> GetAll(string sortOrder, string sortColumn)
     {
         throw new NotImplementedException();
+    }
+
+    public IQueryable<Transaction> GetAll()
+    {
+        return transactionRepository.GetAllAsync();
     }
 
     public async Task<bool?> DeleteAsync(Transaction model)
