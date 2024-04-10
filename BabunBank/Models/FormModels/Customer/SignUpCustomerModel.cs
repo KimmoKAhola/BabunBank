7c79029c83e42a8cbafa08c4e8755e4ab39bbcdd
kahol <kahola@gmail.com>
2024-04-10 16:34:47 +0200

    I have added displayname annotation to my SignUpCustomerModel.cs

--- BabunBank/Models/FormModels/Customer/SignUpCustomerModel.cs
+++ BabunBank/Models/FormModels/Customer/SignUpCustomerModel.cs
@@ -1,57 +1,71 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
-namespace BabunBank.Models.Customer;
+namespace BabunBank.Models.FormModels.Customer;
 
 public class SignUpCustomerModel
 {
     [Required(ErrorMessage = "The gender is a required field.")]
     [StringLength(6, MinimumLength = 2, ErrorMessage = "Please enter between 2-6 characters.")]
+    [DisplayName("Gender")]
     public string Gender { get; init; } = null!;
 
     [Required]
     [StringLength(100, MinimumLength = 2, ErrorMessage = "Please enter at least 2 characters.")]
+    [DisplayName("First Name")]
     public string GivenName { get; init; } = null!;
 
     [Required]
     [StringLength(100, MinimumLength = 2, ErrorMessage = "Please enter at least 2 characters.")]
+    [DisplayName("Last Name")]
     public string SurName { get; init; } = null!;
 
     [Required]
     [StringLength(100, MinimumLength = 2, ErrorMessage = "Please enter at least 2 characters.")]
+    [DisplayName("Street Address")]
     public string StreetAddress { get; init; } = null!;
 
     [Required]
     [StringLength(100, MinimumLength = 2, ErrorMessage = "Please enter at least 2 characters.")]
+    [DisplayName("City")]
     public string City { get; init; } = null!;
 
     [Required]
     [StringLength(15, MinimumLength = 2, ErrorMessage = "Please enter at least 2 characters.")]
+    [DisplayName("Zip Code")]
     public string Zipcode { get; init; } = null!;
 
     [Required]
     [StringLength(100, MinimumLength = 2, ErrorMessage = "Please enter at least 2 characters.")]
+    [DisplayName("Country")]
     public string Country { get; init; } = null!;
 
     [Required]
     [StringLength(2, ErrorMessage = "A country code can only consist of 2 characters.")]
+    [DisplayName("Country Code")]
     public string CountryCode { get; init; } = null!;
 
+    [DisplayName("Date of Birth")]
     public DateOnly? Birthday { get; init; }
 
     [StringLength(20, MinimumLength = 2, ErrorMessage = "Please enter at least 2 characters.")]
+    [DisplayName("National Id")]
     public string? NationalId { get; init; }
 
     [RegularExpression(@"^\+[0-9]+$", ErrorMessage = "Only numbers are allowed after the + sign.")]
     [StringLength(10, ErrorMessage = "Please enter at least 2 characters, starting with a +.")]
+    [DisplayName("Phone Country Code")]
     public string? TelephoneCountryCode { get; init; }
 
     [StringLength(25, MinimumLength = 2)]
     [RegularExpression(@"^\d{2,}$", ErrorMessage = "Only numbers are allowed.")]
+    [DisplayName("Phone Number")]
     public string? TelephoneNumber { get; init; }
 
     [
-        StringLength(20, ErrorMessage = "Please enter a valid email"),
+        StringLength(100, ErrorMessage = "Please enter a valid email"),
         RegularExpression(@"^[a-öA-Ö0-9_.-]{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}$")
     ]
+    [DisplayName("Email Address")]
     public string? EmailAddress { get; init; }
 }
