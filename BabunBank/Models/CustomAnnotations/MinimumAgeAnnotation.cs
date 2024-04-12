05d1f39b3d82b928d5cf9120fbe5e5402f5ef857
kahol <kahola@gmail.com>
2024-04-12 22:19:57 +0200

    Created MinimumAgeAnnotation.cs. This checks that the user is over 18 years old when registering

---
+++ BabunBank/Models/CustomAnnotations/MinimumAgeAnnotation.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabunBank.Models.CustomAnnotations;
+
+public class MinimumAgeAnnotation : ValidationAttribute
+{
+    private const int MinimumAge = 18;
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not DateOnly birthDay)
+            return new ValidationResult("Please enter a valid date format.");
+
+        if (birthDay.AddYears(MinimumAge) <= DateOnly.FromDateTime(DateTime.Now))
+        {
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult("You have to be at least 18 years old to register.");
+    }
+}
