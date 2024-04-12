3089fa1e3c2c17b431c6127993910e4b4db09f49
kahol <kahola@gmail.com>
2024-04-12 22:14:41 +0200

    added a pie plot in DataVisualizationService.cs

--- DetectMoneyLaundering/Services/DataVisualizationService.cs
+++ DetectMoneyLaundering/Services/DataVisualizationService.cs
@@ -5,10 +5,14 @@ namespace DetectMoneyLaundering.Services;
 
 public static class DataVisualizationService
 {
-    public static void CreatePlot(InspectAccountModel model)
+    private static readonly string _goodColor = "#169DE9";
+    private static readonly string _badColor = "#ef4444";
+
+    public static void CreateIndividualPlot(InspectAccountModel model)
     {
         Plot myPlot = new();
         Plot myPlot2 = new();
+        Plot myPlot3 = new();
         var length = model.TransactionsOverLimit.Count(x => x.Amount > 15000);
         var length2 = model.NormalTransactions.Count;
         var xs = new DateTime[length];
@@ -29,25 +33,62 @@ public static class DataVisualizationService
             counter++;
         }
 
+        //Bad plot
         var scatter = myPlot.Add.ScatterPoints(xs, ys);
         scatter.MarkerStyle.Shape = MarkerShape.FilledCircle;
         scatter.MarkerStyle.Size = 6;
-        scatter.MarkerStyle.Fill.Color = Color.FromHex("#ef4444");
+        scatter.MarkerStyle.Fill.Color = Color.FromHex(_badColor);
         myPlot.Axes.DateTimeTicksBottom();
         myPlot.Title($"All suspicious transactions for the customer {model.CustomerName}");
         myPlot.XLabel("Date");
         myPlot.YLabel("Transaction amount in SEK");
         myPlot.SavePng("wwwroot/images/moneylaundering/suscpicious-transactions.png", 800, 800);
 
+        //Good plot
         var scatter2 = myPlot2.Add.ScatterPoints(xs2, ys2);
         scatter2 = myPlot2.Add.ScatterPoints(xs2, ys2);
         scatter2.MarkerStyle.Shape = MarkerShape.FilledCircle;
         scatter2.MarkerStyle.Size = 6;
-        scatter2.MarkerStyle.Fill.Color = Color.FromHex("#169DE9");
+        scatter2.MarkerStyle.Fill.Color = Color.FromHex(_goodColor);
         myPlot2.Axes.DateTimeTicksBottom();
         myPlot2.Title($"All normal transactions for the customer {model.CustomerName}");
         myPlot2.XLabel("Date");
         myPlot2.YLabel("Transaction amount in SEK");
         myPlot2.SavePng("wwwroot/images/moneylaundering/normal-transactions.png", 800, 800);
+
+        var percentageOfSuspiciousTransactions = length / (length + (double)length2);
+
+        //Pie chart
+        List<PieSlice> pieSlices =
+        [
+            new PieSlice
+            {
+                Value = percentageOfSuspiciousTransactions,
+                FillColor = Color.FromHex(_badColor),
+                Label = $"Suspicious transactions - {percentageOfSuspiciousTransactions:P2}"
+            },
+            new PieSlice
+            {
+                Value = 1 - percentageOfSuspiciousTransactions,
+                FillColor = Color.FromHex(_goodColor),
+                Label = $"Normal transactions - {1 - percentageOfSuspiciousTransactions:P2}"
+            }
+        ];
+
+        var piePlot = myPlot3.Add.Pie(pieSlices);
+        piePlot.DonutFraction = .5;
+        piePlot.ExplodeFraction = 0.1;
+
+        myPlot3.ShowLegend();
+
+        myPlot3.Title($"Percentage overview of transactions for the customer {model.CustomerName}");
+        myPlot3.SavePng("wwwroot/images/moneylaundering/piechart.png", 800, 800);
+    }
+
+    public static void CreateGeneralPlot()
+    {
+        Plot myPlot = new();
+
+        myPlot.SavePng("wwwroot/images/moneylaundering/bigplot.png", 800, 800);
     }
 }
