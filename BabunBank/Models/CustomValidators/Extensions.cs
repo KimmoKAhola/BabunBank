f09e225b3c0d206eb74bd8c69d7b3dc5383b44c6
kahol <kahola@gmail.com>
2024-04-12 22:17:07 +0200

    Created a helper method to display all validation errors for my modelstates in Extensions.cs

---
+++ BabunBank/Models/CustomValidators/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BabunBank.Models.CustomValidators;
+
+public static class Extensions
+{
+    public static void AddToModelState(
+        this ValidationResult result,
+        ModelStateDictionary modelState
+    )
+    {
+        foreach (var error in result.Errors)
+        {
+            modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+        }
+    }
+}
