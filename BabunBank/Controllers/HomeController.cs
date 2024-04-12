e2768034234b5b93ba2966f054b5a410d9735581
kahol <kahola@gmail.com>
2024-04-12 22:17:39 +0200

    Cleaned some code in HomeController.cs
    Also added contactusValidator to the class for my modelstate

--- BabunBank/Controllers/HomeController.cs
+++ BabunBank/Controllers/HomeController.cs
@@ -1,27 +1,20 @@
 using System.Diagnostics;
-using BabunBank.Models;
+using BabunBank.Models.CustomValidators;
 using BabunBank.Models.FormModels.Home;
 using BabunBank.Models.ViewModels.Error;
 using BabunBank.Services;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BabunBank.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(
+        LandingPageService landingPageService,
+        ContactUsValidator contactUsValidator
+    ) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly LandingPageService _landingPageService;
-
-        public HomeController(ILogger<HomeController> logger, LandingPageService landingPageService)
-        {
-            _logger = logger;
-            _landingPageService = landingPageService;
-        }
-
         public async Task<IActionResult> Index()
         {
-            var landingPageViewModel = await _landingPageService.GetLandingPageInfo();
+            var landingPageViewModel = await landingPageService.GetLandingPageInfo();
 
             return View(landingPageViewModel);
         }
@@ -42,11 +35,13 @@ namespace BabunBank.Controllers
             [Bind("FirstName", "LastName", "Email", "Message")] ContactUsModel model
         )
         {
-            if (!ModelState.IsValid)
-                return View(model);
+            var validationResult = contactUsValidator.Validate(model);
 
-            //TODO Add something nice here!
-            return RedirectToAction("Contact");
+            if (validationResult.IsValid)
+                return RedirectToAction("Contact");
+
+            validationResult.AddToModelState(ModelState);
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
