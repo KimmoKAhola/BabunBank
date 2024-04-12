75d4fd0fc166dbd1bcefe38e101de96a6f047d59
kahol <kahola@gmail.com>
2024-04-12 22:04:28 +0200

    Removed annotations from SignUpUserModel.cs

--- BabunBank/Models/FormModels/User/SignUpUserModel.cs
+++ BabunBank/Models/FormModels/User/SignUpUserModel.cs
@@ -1,22 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
-using DataAccessLibrary.Data;
+﻿using DataAccessLibrary.Data;
 
 namespace BabunBank.Models.FormModels.User;
 
 public class SignUpUserModel
 {
-    [Required]
     public string UserName { get; init; } = null!;
 
-    [Required]
     public string Email { get; init; } = null!;
 
-    [Required]
     public string Password { get; init; } = null!;
 
-    [Required]
     public bool EmailConfirmed { get; init; } = true;
 
-    [Required]
     public UserRole UserRole { get; init; }
 }
