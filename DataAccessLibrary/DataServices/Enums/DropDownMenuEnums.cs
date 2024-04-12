5b41c1ab67f897c8950bae65f1755efae548d66c
kahol <kahola@gmail.com>
2024-04-12 22:15:11 +0200

    Created DropDownMenuEnums.cs, enums for my different dropdowns

---
+++ DataAccessLibrary/DataServices/Enums/DropDownMenuEnums.cs
@@ -0,0 +1,24 @@
+﻿namespace DataAccessLibrary.DataServices.Enums;
+
+public enum GenderOptions
+{
+    Choose = 0,
+    Male = 1,
+    Female = 2,
+    Other = 3
+}
+
+public enum UserRoles
+{
+    Admin,
+    Cashier
+}
+
+public enum CountryOptions
+{
+    Choose,
+    Denmark,
+    Finland,
+    Norway,
+    Sweden
+}
