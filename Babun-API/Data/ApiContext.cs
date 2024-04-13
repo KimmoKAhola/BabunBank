cdd5cdaa7fee688b067039db412545cb4377e48c
kahol <kahola@gmail.com>
2024-04-13 22:06:21 +0200

    created ApiContext.cs for my api. contains one table only, Ads.

---
+++ Babun-API/Data/ApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Babun_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Babun_API.Data;
+
+public class ApiContext : DbContext
+{
+    protected ApiContext() { }
+
+    public ApiContext(DbContextOptions<ApiContext> options)
+        : base(options) { }
+
+    public DbSet<Ad> Ads { get; init; }
+}
