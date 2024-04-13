e18f49c9485cd088f2018faf670d17ff018910e8
kahol <kahola@gmail.com>
2024-04-13 22:13:22 +0200

    Created a datamodel for my blogposts

---
+++ BabunBank/Models/ViewModels/ApiBlog/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿namespace BabunBank.Models.ViewModels.ApiBlog;
+
+public class BlogPost
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Description { get; set; }
+    public string Content { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime? LastModified { get; set; }
+}
