0af5c9cd7940a46226fd6e2e31c1e4ecc762fab8
kahol <kahola@gmail.com>
2024-04-16 22:17:13 +0200

    Refactor AccountController and improve error handling
    The AccountController class in the Babun-API project has been refactored for clarity and readability. Documentation comments have been added for better understanding of the class purpose and method functionalities. Additionally, error handling mechanisms have been improved to manage potential issues when retrieving account data from the database more effectively.

--- Babun-API/Controllers/AccountController.cs
+++ Babun-API/Controllers/AccountController.cs
@@ -1,21 +1,40 @@
 using DataAccessLibrary.DataServices;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Babun_API.Controllers
+namespace Babun_API.Controllers;
+
+/// <summary>
+/// AccountController class handles API endpoints related to account operations.
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController(DataAccountService dataAccountService) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController(DataAccountService dataAccountService) : ControllerBase
+    /// <summary>
+    /// Retrieves an account by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the account to retrieve.</param>
+    /// <returns>The account with the specified ID.</returns>
+    [HttpGet]
+    public async Task<IActionResult> Get(int id)
     {
-        [HttpGet]
-        public async Task<IActionResult> Get(int id)
+        try
         {
-            var test = await dataAccountService.GetAsync(id);
+            var account = await dataAccountService.GetAsync(id);
+            if (account == null)
+            {
+                return NotFound($"Account with ID {id} not found");
+            }
 
-            Console.WriteLine(test);
-
-            return Ok(test.AccountId);
+            return Ok(account.AccountId);
+        }
+        catch (Exception ex)
+        {
+            // Log the exception here
+            return StatusCode(
+                StatusCodes.Status500InternalServerError,
+                $"Error retrieving data from the database"
+            );
         }
     }
 }
