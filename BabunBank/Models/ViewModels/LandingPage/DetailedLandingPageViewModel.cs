a87de85ee5c5b2cc72a98bfebf5ede63e45ef28e
kahol <kahola@gmail.com>
2024-04-19 13:28:35 +0200

    Add DetailedLandingPageViewModel for landing page
    Implemented a new View Model, DetailedLandingPageViewModel, in Models. This ViewModel will be used for displaying detailed information on the landing page. It includes properties such as CustomerId, AccountId, Name, Country, and AccountSum.

---
+++ BabunBank/Models/ViewModels/LandingPage/DetailedLandingPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BabunBank.Models.ViewModels.LandingPage;
+
+public record DetailedLandingPageViewModel
+{
+    public int CustomerId { get; init; }
+    public int AccountId { get; init; }
+    public string Name { get; init; } = null!;
+    public string Country { get; init; } = null!;
+    public decimal AccountSum { get; init; }
+}
