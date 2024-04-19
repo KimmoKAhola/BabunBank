6d1e58869eee634940de770da3d2a883e231429b
kahol <kahola@gmail.com>
2024-04-19 13:27:50 +0200

    Update GetLandingPageQuery method in DataLandingPageService
    The GetLandingPageQuery method has been significantly updated. Instead of returning an IEnumerable of Accounts, it's been changed to return a detailed summary of accounts by country, including the number of accounts, total balance, and number of customers. A new method, GetDetailedLandingPageQuery, has also been added for specific information per country.

--- DataAccessLibrary/DataServices/DataLandingPageService.cs
+++ DataAccessLibrary/DataServices/DataLandingPageService.cs
@@ -6,7 +6,55 @@ namespace DataAccessLibrary.DataServices;
 
 public class DataLandingPageService(BankAppDataContext dbContext)
 {
-    public async Task<IEnumerable<Account>> GetLandingPageQuery()
+    public async Task<
+        IEnumerable<(
+            string country,
+            int NumberOfAccounts,
+            decimal TotalBalancePerCountry,
+            int NumberOfCustomers
+        )>
+    > GetLandingPageQuery()
+    {
+        var query = await dbContext
+            .Dispositions.Include(d => d.Customer)
+            .Include(x => x.Customer.Dispositions)
+            .ThenInclude(x => x.Account)
+            .ThenInclude(x => x.Transactions)
+            .GroupBy(x => x.Customer.Country)
+            .Select(x => new
+            {
+                Country = x.Key,
+                NumberOfAccounts = x.SelectMany(d => d.Customer.Dispositions)
+                    .Select(d => d.Account)
+                    .Distinct()
+                    .Count(),
+                TotalBalancePerCountry = x.SelectMany(d => d.Account.Transactions)
+                    .Sum(t => t.Amount),
+                NumberOfCustomers = x.Select(d => d.Customer).Distinct().Count()
+            })
+            .ToListAsync();
+
+        var result = query.Select(x =>
+            (
+                x.Country,
+                TotalAccounts: x.NumberOfAccounts,
+                TotalTransactions: x.TotalBalancePerCountry,
+                TotalCustomers: x.NumberOfCustomers
+            )
+        );
+
+        return result;
+    }
+
+    public async Task<
+        IEnumerable<(
+            string Country,
+            int CustomerId,
+            int AccountId,
+            decimal TotalBalance,
+            string CustomerName
+        )>
+    > GetDetailedLandingPageQuery(string country)
     {
         var data = await dbContext.Accounts
             .Include(x => x.Dispositions)
