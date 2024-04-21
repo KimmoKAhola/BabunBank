882cea2d485dda1af4662d3bbe6d5cac477021a7
kahol <kahola@gmail.com>
2024-04-21 18:09:53 +0200

    Add MaximumAmount custom validation attribute
    A new custom validation attribute called MaximumAmount has been added for comparing properties. This attribute, which is mainly useful in context of amount validation, checks if a given amount is greater than a comparison value, for instance, to prevent exceeding available balance.

---
+++ BabunBank/Models/CustomAnnotations/MaximumAmount.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabunBank.Models.CustomAnnotations;
+
+public class MaximumAmount(string comparisonProperty) : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var comparisonPropertyInfo = validationContext.ObjectType.GetProperty(comparisonProperty);
+
+        if (comparisonPropertyInfo == null || value == null)
+        {
+            return new ValidationResult($"Property {comparisonProperty} not found.");
+        }
+
+        var comparisonValue = (decimal?)
+            comparisonPropertyInfo.GetValue(validationContext.ObjectInstance);
+
+        if (comparisonValue == null)
+            return new ValidationResult("The value is not a decimal");
+
+        if (value is not decimal decimalValue)
+            return new ValidationResult("The value is not a decimal");
+
+        return decimalValue > comparisonValue
+            ? new ValidationResult("Amount can not exceed the available balance.")
+            : ValidationResult.Success;
+    }
+}
