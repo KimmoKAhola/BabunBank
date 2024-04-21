0d5dcce3a78cd844923c415b5f68cbbea3c3ec3c
kahol <kahola@gmail.com>
2024-04-21 14:47:26 +0200

    Add new using directives in SignUpCustomerModel.cs
    The code for SignUpCustomerModel.cs has been updated with two new using directives. These imports, DataAccessLibrary.Data and Microsoft.AspNetCore.Mvc, were added to help handle data access and controller functionalities respectively.

--- BabunBank/Models/FormModels/Customer/SignUpCustomerModel.cs
+++ BabunBank/Models/FormModels/Customer/SignUpCustomerModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using BabunBank.Models.CustomAnnotations;
+using DataAccessLibrary.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BabunBank.Models.FormModels.Customer;
 
