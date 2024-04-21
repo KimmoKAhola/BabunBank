9886bfda3cb52b5b1db0dd983578214be1781e26
kahol <kahola@gmail.com>
2024-04-21 14:44:41 +0200

    Add AccountFactory for creating new accounts
    This commit introduces a new AccountFactory that helps in the creation of new Account instances. Default values for 'Frequency', 'Created', and 'Balance' attributes are set within this factory method.

---
+++ BabunBank/Factories/AccountFactory.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLibrary.Data;
+
+namespace BabunBank.Factories;
+
+public static class AccountFactory
+{
+    public static Account CreateNewAccount()
+    {
+        var account = new Account
+        {
+            Frequency = "AfterTransaction",
+            Created = DateOnly.FromDateTime(DateTime.Now),
+            Balance = 0m
+        };
+
+        return account;
+    }
+}
