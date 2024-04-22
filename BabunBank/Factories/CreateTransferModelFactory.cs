69537105466bf2be3732448a845b787f6449e6c7
kahol <kahola@gmail.com>
2024-04-22 15:38:32 +0200

    Add CreateTransferModelFactory class
    This commit introduces a new class, CreateTransferModelFactory. This class is used to create models for Deposit, Withdrawal, and Transfer operations, taking the account data as an input and using the current date for transaction logging.

---
+++ BabunBank/Factories/CreateTransferModelFactory.cs
@@ -0,0 +1,49 @@
+﻿using BabunBank.Models.FormModels.Cashier;
+using BabunBank.Models.ViewModels.Account;
+
+namespace BabunBank.Factories;
+
+public static class CreateTransferModelFactory
+{
+    public static CreateDepositModel CreateDepositModel(AccountViewModel accountViewModel)
+    {
+        var model = new CreateDepositModel
+        {
+            AccountId = accountViewModel.AccountId,
+            Balance = accountViewModel.Balance,
+            Date = DateOnly.FromDateTime(DateTime.Now),
+            Type = "Debit",
+            Operation = "Deposit"
+        };
+
+        return model;
+    }
+
+    public static CreateWithdrawalModel CreateWithdrawalModel(AccountViewModel accountViewModel)
+    {
+        var model = new CreateWithdrawalModel
+        {
+            AccountId = accountViewModel.AccountId,
+            Balance = accountViewModel.Balance,
+            Date = DateOnly.FromDateTime(DateTime.Now),
+            Type = "Debit",
+            Operation = "Withdrawal"
+        };
+
+        return model;
+    }
+
+    public static CreateTransferModel CreateTransferModel(AccountViewModel sender)
+    {
+        var model = new CreateTransferModel
+        {
+            FromAccountId = sender.CustomerId,
+            BalanceSender = sender.Balance,
+            Date = DateOnly.FromDateTime(DateTime.Now),
+            OperationSender = "Transfer to",
+            OperationReceiver = "Deposit from"
+        };
+
+        return model;
+    }
+}
