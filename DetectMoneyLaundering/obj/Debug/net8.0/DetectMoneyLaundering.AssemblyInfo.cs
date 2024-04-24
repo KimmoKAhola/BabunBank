16a0c3652b349146a7f0154d0f98a0f33501a1ed
kahol <kahola@gmail.com>
2024-04-24 09:32:56 +0200

    Update assembly informational versions and source link references
    This commit updated the assembly informational versions in several files, reflecting the change in the git revision hash. The source link references used for debugging have also been updated to the new revision hash. Additionally, a new data source was added in the `dataSources.xml` which will connect to a database on azure.

--- DetectMoneyLaundering/obj/Debug/net8.0/DetectMoneyLaundering.AssemblyInfo.cs
+++ DetectMoneyLaundering/obj/Debug/net8.0/DetectMoneyLaundering.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7a80001495ca94f416d088ae57f746590aacb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16173924b39fefa510d148fae1a280a5ce12660a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
