a2e03dd2eda0b04ff6b797719238fa79bdd8102f
kahol <kahola@gmail.com>
2024-04-24 18:51:55 +0200

    Refactor TokenController and add login functionality
    The TokenController class has been refactored for better code organization, readability and maintenance. Two additional functions have been added for loading configurations and generating security token descriptor. The refactoring also includes adding a login functionality, where a user login is authenticated and if successful, a token is generated and returned.

--- Babun-API/Controllers/TokenController.cs
+++ Babun-API/Controllers/TokenController.cs
@@ -1,8 +1,10 @@
 using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 using System.Text;
 using Asp.Versioning;
+using Babun_API.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Babun_API.Controllers;
@@ -10,11 +12,12 @@ namespace Babun_API.Controllers;
 /// <summary>
 /// This class generates a token that is needed for authorization in the API.
 /// </summary>
-[ApiVersion("1.0")]
-[ApiExplorerSettings(GroupName = "v1")]
-[Route("[controller]")]
+[Route("/[action]")]
 [ApiController]
-public class TokenController : ControllerBase
+public class TokenController(
+    SignInManager<IdentityUser> signInManager,
+    UserManager<IdentityUser> userManager
+) : ControllerBase
 {
     private static readonly TimeSpan TokenLifeTime = TimeSpan.FromMinutes(10);
 
@@ -25,18 +28,36 @@ public class TokenController : ControllerBase
     /// <returns>
     /// A token is returned as a simple string. Lasts for 10 minutes.
     /// </returns>
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(GroupName = "v1")]
     [ProducesResponseType(typeof(string), 200)]
     [HttpGet]
     public IActionResult GenerateToken()
     {
-        var config = new ConfigurationBuilder()
-            .AddJsonFile("apiAppsettings.json", false, true)
-            .Build();
-        var section = config.GetSection("JwtSettings");
+        var tokenString = GenerateTokenString();
+
+        return Ok(tokenString);
+    }
+
+    private static string GenerateTokenString()
+    {
+        var section = LoadJwtSettings();
 
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(section["Key"]!);
 
+        var tokenDescriptor = SecurityTokenDescriptor(section, key);
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var tokenString = tokenHandler.WriteToken(token);
+        return tokenString;
+    }
+
+    private static SecurityTokenDescriptor SecurityTokenDescriptor(
+        IConfiguration section,
+        byte[] key
+    )
+    {
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Issuer = section["Issuer"],
@@ -47,10 +68,64 @@ public class TokenController : ControllerBase
                 SecurityAlgorithms.HmacSha256Signature
             )
         };
+        return tokenDescriptor;
+    }
 
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-        var tokenString = tokenHandler.WriteToken(token);
+    private static IConfigurationSection LoadJwtSettings()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("apiAppsettings.json", false, true)
+            .Build();
+        var section = config.GetSection("JwtSettings");
+        return section;
+    }
 
-        return Ok(tokenString);
+    /// <summary>
+    /// Logs in a user and generates a token for authorization in the API.
+    /// </summary>
+    /// <param name="login">The login information of the user.</param>
+    /// <returns>
+    /// If the login is successful, a token is returned as a simple string.
+    /// </returns>
+    [ApiVersion("2.0")]
+    [ApiExplorerSettings(GroupName = "v2")]
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginModel login)
+    {
+        try
+        {
+            var userExists = await userManager.FindByEmailAsync(login.Username);
+
+            if (userExists == null)
+            {
+                return Unauthorized();
+            }
+
+            var signInResult = await signInManager.CheckPasswordSignInAsync(
+                userExists,
+                login.Password,
+                lockoutOnFailure: false
+            );
+
+            if (!signInResult.Succeeded)
+            {
+                return Unauthorized();
+            }
+
+            var token = GenerateTokenString();
+
+            return Ok(token);
+        }
+        catch (Exception e)
+        {
+            return Ok(e + "\n" + e.Message + "\n" + e.InnerException);
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Test()
+    {
+        var res = await userManager.Users.ToListAsync();
+        return Ok(res);
     }
 }
