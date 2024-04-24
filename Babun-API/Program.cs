39766205905d13e12319ebb0b10b272b4bdbd645
kahol <kahola@gmail.com>
2024-04-24 21:14:15 +0200

    Refactor code by implementing configuration classes
    The update involves moving service and Swagger configuration code to dedicated configuration classes, thus reducing the complexity and length of the Program.cs file. The configuration tasks now done in ServiceConfigurations and SwaggerConfiguration classes include services registration, Swagger documentation setup, Swagger authentication and authorization process, and Swagger API versioning.

--- Babun-API/Program.cs
+++ Babun-API/Program.cs
@@ -1,197 +1,19 @@
-using System.Reflection;
-using System.Text;
-using Asp.Versioning;
-using BabunBank.Services;
-using Babun_API;
-using Babun_API.Data;
-using DataAccessLibrary.Data;
-using DataAccessLibrary.DataServices;
-using DataAccessLibrary.Repositories;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using Babun_API.Infrastructure.Configurations;
 using DataInitializer = Babun_API.Data.DataInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+ServiceConfigurations.RegisterServices(builder);
 
-builder.Configuration.AddJsonFile("apiAppsettings.json", false, true);
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var secondConnectionString = builder.Configuration.GetConnectionString("SecondaryConnection");
-
-builder.Services.AddDbContext<ApiContext>(opt => opt.UseSqlServer(connectionString));
-builder.Services.AddDbContext<BankAppDataContext>(opt => opt.UseSqlServer(connectionString));
-
-builder
-    .Services.AddDefaultIdentity<IdentityUser>()
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<BankAppDataContext>();
-
-builder.Services.AddScoped<AccountRepository>();
-builder.Services.AddScoped<TransactionRepository>();
-
-builder.Services.AddScoped<DataAccountService>();
-builder.Services.AddScoped<DataTransactionService>();
-
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc(
-        "v1",
-        new OpenApiInfo
-        {
-            Version = "v1",
-            Title = "Babun Bank News API",
-            Description =
-                "An api for reading and creating news."
-                + "\nAny news created here can be viewed at https://babunbank.azurewebsites.net/"
-                + "\nPlease do not type antything racist.",
-            TermsOfService = new Uri("https://example.com/contact"),
-            Contact = new OpenApiContact
-            {
-                Name = "Example contact",
-                Url = new Uri("https://example.com/contact"),
-                Email = ""
-            },
-            License = new OpenApiLicense
-            {
-                Name = "",
-                Url = new Uri("https://example.com/license"),
-            }
-        }
-    );
-
-    options.AddSecurityDefinition(
-        "Bearer",
-        new OpenApiSecurityScheme
-        {
-            Description =
-                "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\"",
-            Type = SecuritySchemeType.Http,
-            Scheme = "bearer",
-            BearerFormat = "JWT"
-        }
-    );
-
-    options.AddSecurityRequirement(
-        new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                },
-                new string[] { }
-            }
-        }
-    );
-
-    options.SwaggerDoc(
-        "v2",
-        new OpenApiInfo
-        {
-            Version = "v2",
-            Title = "Babun Bank News API - v2",
-            Description =
-                "An api for reading and creating news. Version 2! To access this api you need to use the Babun Bank Website."
-                + "\nThe token is hidden and the token generator will not work for this version of the api."
-                + "\nhttps://babunbank.azurewebsites.net/ to see the results.",
-            TermsOfService = new Uri("https://example.com/contact"),
-            Contact = new OpenApiContact
-            {
-                Name = "Example contact",
-                Url = new Uri("https://example.com/contact"),
-                Email = ""
-            },
-            License = new OpenApiLicense { Name = "", Url = new Uri("https://example.com/contact") }
-        }
-    );
-
-    options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
-    options.CustomSchemaIds(x => x.Name);
-
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
-
-builder
-    .Services.AddAuthentication("V1Scheme")
-    .AddJwtBearer(
-        "V1Scheme",
-        x =>
-        {
-            var settings = builder.Configuration.GetSection("JwtSettings");
-            x.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidIssuer = settings["Issuer"],
-                ValidAudience = settings["Audience"],
-                IssuerSigningKey = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(settings["Key"]!)
-                ),
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ClockSkew = TimeSpan.Zero,
-                ValidateIssuerSigningKey = true
-            };
-        }
-    )
-    .AddJwtBearer(
-        "V2Scheme",
-        options =>
-        {
-            options.RequireHttpsMetadata = false;
-            var settings = builder.Configuration.GetSection("JwtSettings:V2");
-            options.SaveToken = true;
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidIssuer = settings["Issuer"],
-                ValidAudience = settings["Audience"],
-                IssuerSigningKey = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(settings["Key"]!)
-                ),
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ClockSkew = TimeSpan.Zero,
-                RoleClaimType = "V2Claim"
-            };
-        }
-    );
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy(
-        "V2Policy",
-        policy =>
-        {
-            policy.RequireClaim("V2Claim");
-        }
-    );
-});
+SwaggerConfiguration.AddSwaggerDocumentation(builder);
+SwaggerConfiguration.AddSwaggerAuthentication(builder);
+SwaggerConfiguration.AddSwaggerAuthorization(builder);
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 
-builder.Services.AddApiVersioning(o =>
-{
-    o.DefaultApiVersion = new ApiVersion(1, 0);
-    o.AssumeDefaultVersionWhenUnspecified = true;
-    o.ReportApiVersions = true;
-});
+SwaggerConfiguration.AddSwaggerApiVersioning(builder);
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddTransient<DataInitializer>();
 
 var app = builder.Build();
@@ -199,22 +21,11 @@ var app = builder.Build();
 Task.Run(async () =>
     {
         await using var scope = app.Services.CreateAsyncScope();
-        await scope.ServiceProvider.GetService<DataInitializer>().SeedData();
+        await scope.ServiceProvider.GetService<DataInitializer>()!.SeedData();
     })
     .Wait();
 
-app.UseSwagger();
-
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint($"/swagger/v1/swagger.json", "V1.0");
-    options.SwaggerEndpoint($"/swagger/v2/swagger.json", "V2.0");
-
-    options.OAuthClientId("swagger");
-    options.OAuthClientSecret("swagger_secret");
-    options.OAuthAppName("Swagger UI");
-    options.OAuthUsePkce();
-});
+SwaggerConfiguration.UseSwaggerDocumentation(app);
 
 app.UseHttpsRedirection();
 
