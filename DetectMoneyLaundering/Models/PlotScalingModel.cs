ffdd7330d87236e9a33ac5873613a09edb6ea14d
kahol <kahola@gmail.com>
2024-04-28 12:27:39 +0200

    Add PlotScalingModel in DetectMoneyLaundering project
    A new model, PlotScalingModel, has been added to the DetectMoneyLaundering project. This model will be used to manage the scaling factors for plot height, width, font, and color.

---
+++ DetectMoneyLaundering/Models/PlotScalingModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace DetectMoneyLaundering.Models;
+
+public record PlotScalingModel
+{
+    public double HeightScaleFactor { get; init; }
+    public double WidthScaleFactor { get; init; }
+    public double FontScaleFactor { get; init; }
+
+    public Color Color { get; set; }
+}
