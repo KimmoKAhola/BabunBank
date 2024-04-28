12156b389afa2d7fd20189959449929401b522c9
kahol <kahola@gmail.com>
2024-04-28 19:58:55 +0200

    Update static web assets and assembly info
    This update includes the addition of new static web assets to the BabunBank project. Changes have been made to the staticwebassets.build.json file and the BabunBank.Microsoft.AspNetCore.StaticWebAssets.props file to include new images. The assembly version information in Babun-API.AssemblyInfo.cs has also been updated.

--- Babun-API/obj/Debug/net8.0/Babun-API.AssemblyInfo.cs
+++ Babun-API/obj/Debug/net8.0/Babun-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Babun-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec798cec197e1205c15eb6c95e149e08964ba84f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe7f2c925a421a866dcabfa345f931173014c96")]
 [assembly: System.Reflection.AssemblyProductAttribute("Babun-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Babun-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DetectMoneyLaundering/obj/Debug/net8.0/DetectMoneyLaundering.AssemblyInfo.cs
+++ DetectMoneyLaundering/obj/Debug/net8.0/DetectMoneyLaundering.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec798cec197e1205c15eb6c95e149e08964ba84f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe7f2c925a421a866dcabfa345f931173014c96")]
 [assembly: System.Reflection.AssemblyProductAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
