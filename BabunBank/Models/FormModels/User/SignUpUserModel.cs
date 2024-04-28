bfb115a993e70110f7ba20e2d6bfa3051fb44adc
kahol <kahola@gmail.com>
2024-04-28 19:59:35 +0200

    Add email and password confirmation to SignUpUserModel
    The SignUpUserModel class in the FormModels has been updated to include email and password confirmation. Email and password must now be entered twice during registration, and each pair must match. This ensures user inputs are accurate, improves data integrity, and prevents mistakes during user sign up.

--- BabunBank/Models/FormModels/User/SignUpUserModel.cs
+++ BabunBank/Models/FormModels/User/SignUpUserModel.cs
@@ -1,14 +1,20 @@
-﻿using DataAccessLibrary.Data;
+﻿using System.ComponentModel.DataAnnotations;
+using DataAccessLibrary.Data;
 
 namespace BabunBank.Models.FormModels.User;
 
 public class SignUpUserModel
 {
-    public string UserName { get; init; } = null!;
-
     public string Email { get; init; } = null!;
 
+    [EmailAddress]
+    [Compare("Email", ErrorMessage = "Email addresses has to match")]
+    public string ConfirmEmail { get; init; } = null!;
+
     public string Password { get; init; } = null!;
+
+    [Compare("Password", ErrorMessage = "Passwords has to match")]
+    public string ConfirmPassword { get; init; } = null!;
 
     public bool EmailConfirmed { get; init; } = true;
 
