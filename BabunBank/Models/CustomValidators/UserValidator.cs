130890578adf04b9974cdd462569f9e8f3468e53
kahol <kahola@gmail.com>
2024-04-28 20:07:32 +0200

    Update user email validation and assembly version
    In the UserValidator.cs file, an additional validation was added to ensure the email addresses match. Also, the AssemblyInformationalVersionAttribute in the AssemblyInfo.cs file and the document URL in the sourcelink.json file have been updated to reflect the new commit version.

--- BabunBank/Models/CustomValidators/UserValidator.cs
+++ BabunBank/Models/CustomValidators/UserValidator.cs
@@ -21,7 +21,9 @@ public class UserValidator : AbstractValidator<SignUpUserModel>
             .Length(6, 100)
             .WithMessage("Email has to be at least 6 characters")
             .MustAsync(BeUniqueEmail)
-            .WithMessage("A user with that email already exists.");
+            .WithMessage("A user with that email already exists.")
+            .Equal("Email")
+            .WithMessage("The email addresses has to match.");
 
         RuleFor(user => user.Password).NotNull().NotEmpty();
 
--- BabunBank/obj/Debug/net8.0/BabunBank.AssemblyInfo.cs
+++ BabunBank/obj/Debug/net8.0/BabunBank.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BabunBank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe7f2c925a421a866dcabfa345f931173014c96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c7dff707d52b9ebcb678fca34b32d4fc3a493a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BabunBank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BabunBank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
