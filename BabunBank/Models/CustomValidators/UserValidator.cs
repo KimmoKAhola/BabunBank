376789611507540a71a09c7f950603cb8b4a790f
kahol <kahola@gmail.com>
2024-04-28 20:01:02 +0200

    Remove username validation in UserValidator
    Username-specific rules have been removed from the UserValidator class. This includes checks for null, empty, username length, and uniqueness in the database. Instead, an update was made to the user role validation to ensure a valid option is selected.

--- BabunBank/Models/CustomValidators/UserValidator.cs
+++ BabunBank/Models/CustomValidators/UserValidator.cs
@@ -13,16 +13,6 @@ public class UserValidator : AbstractValidator<SignUpUserModel>
     {
         _identityUserService = identityUserService;
 
-        RuleFor(user => user.UserName)
-            .NotNull()
-            .WithMessage("Username is required")
-            .NotEmpty()
-            .WithMessage("Username is required")
-            .Length(6, 100)
-            .WithMessage("Username must be between 2 and 100 characters")
-            .MustAsync(BeUniqueUserName)
-            .WithMessage("A user with that username already exists");
-
         RuleFor(user => user.Email)
             .NotNull()
             .WithMessage("Field can not be empty.")
@@ -35,16 +25,12 @@ public class UserValidator : AbstractValidator<SignUpUserModel>
 
         RuleFor(user => user.Password).NotNull().NotEmpty();
 
-        RuleFor(user => user.UserRole).NotNull().NotEmpty();
-    }
-
-    private async Task<bool> BeUniqueUserName(string username, CancellationToken cancellationToken)
-    {
-        return await _identityUserService.CheckIfExistsByUsernameAsync(username);
+        RuleFor(user => (int)user.UserRole).NotEqual(0).WithMessage("Please choose a valid option");
     }
 
     private async Task<bool> BeUniqueEmail(string email, CancellationToken cancellationToken)
     {
-        return await _identityUserService.CheckIfExistsByEmailAsync(email);
+        var result = await _identityUserService.CheckIfExistsByEmailAsync(email);
+        return result;
     }
 }
