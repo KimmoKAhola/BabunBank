6f736c155adaec3cd46b10965997b6e7151d84c5
kahol <kahola@gmail.com>
2024-04-28 14:03:35 +0200

    Rename 'Color' property to 'BackgroundColor'
    The 'Color' property in the PlotScalingModel has been renamed to 'BackgroundColor'. This name change better reflects the purpose of the property in the context of the model.

--- DetectMoneyLaundering/Models/PlotScalingModel.cs
+++ DetectMoneyLaundering/Models/PlotScalingModel.cs
@@ -7,6 +7,6 @@ public record PlotScalingModel
     public double HeightScaleFactor { get; init; }
     public double WidthScaleFactor { get; init; }
     public double FontScaleFactor { get; init; }
-
     public Color Color { get; set; }
+    public Color BackgroundColor { get; set; }
 }
