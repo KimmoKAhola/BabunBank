fc892bd7a895aab44e7d6e64205081c2b86a7192
kahol <kahola@gmail.com>
2024-04-29 13:44:10 +0200

    Add HttpConfiguration and HttpClientOptions classes
    Added HttpConfiguration in the Dependencies folder, which is responsible for the configuration of Http services including HttpClient. Also introduced a new record type, HttpClientOptions, to handle HttpClient base address configuration. These changes improve the system's configurability and maintenance.

---
+++ BabunBank/Infrastructure/Configurations/Dependencies/HttpConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using BabunBank.Infrastructure.Configurations.HttpClients;
+using BabunBank.Infrastructure.Interfaces;
+using BabunBank.Models.ViewModels.ApiBlog;
+using Microsoft.Extensions.Options;
+
+namespace BabunBank.Infrastructure.Configurations.Dependencies;
+
+public static class HttpConfiguration
+{
+    public static void Configuration(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<NewsHttpClientEndPoints>();
+        services.Configure<HttpClientOptions>(configuration.GetSection("HttpClientOptions"));
+        services.AddHttpClient<INewsHttpClient<BlogPost>, NewsHttpClient>(
+            (services, options) =>
+            {
+                var httpClientOptions = services
+                    .GetRequiredService<IOptions<HttpClientOptions>>()
+                    .Value;
+                options.BaseAddress = new Uri(httpClientOptions.BaseAddress);
+            }
+        );
+        services.Configure<NewsHttpClientEndPoints>(configuration.GetSection("ServiceEndPoints"));
+    }
+}
---
+++ BabunBank/Infrastructure/Configurations/HttpClients/HttpClientOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace BabunBank.Infrastructure.Configurations.HttpClients;
+
+public record HttpClientOptions
+{
+    public string BaseAddress { get; init; } = null!;
+}
