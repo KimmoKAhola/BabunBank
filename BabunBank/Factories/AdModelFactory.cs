4e6a0d570c8de09094e35c15f3e2f3bd70019975
kahol <kahola@gmail.com>
2024-04-29 12:17:48 +0200

    Add AdModelFactory for creating EditAdModel
    A new factory called AdModelFactory has been added to build EditAdModel instances. This factory takes a BlogPost object and derives the corresponding fields to create a new EditAdModel, setting the IsDeleted field to false by default.

---
+++ BabunBank/Factories/AdModelFactory.cs
@@ -0,0 +1,19 @@
+﻿using BabunBank.Models.FormModels.AdModels;
+using BabunBank.Models.ViewModels.ApiBlog;
+
+namespace BabunBank.Factories;
+
+public static class AdModelFactory
+{
+    public static EditAdModel Create(BlogPost blogpost)
+    {
+        return new EditAdModel
+        {
+            Title = blogpost.Title,
+            Author = blogpost.Author,
+            Content = blogpost.Content,
+            Description = blogpost.Description,
+            IsDeleted = false
+        };
+    }
+}
