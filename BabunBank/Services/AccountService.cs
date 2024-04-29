9025fd648c38c34aa9bbb4c50205d3cf665551ab
kahol <kahola@gmail.com>
2024-04-29 12:53:13 +0200

    Implement IAccountService in AccountService
    AccountService class now implements the IAccountService interface, following the change in code. This aligns with our strategy to encourage better code organization and enforcement of coding standards through interfaces.

--- BabunBank/Services/AccountService.cs
+++ BabunBank/Services/AccountService.cs
@@ -1,11 +1,12 @@
 ﻿using AutoMapper;
+using BabunBank.Configurations.Interfaces;
 using BabunBank.Models.ViewModels.Account;
 using DataAccessLibrary.Data;
 using DataAccessLibrary.DataServices;
 
 namespace BabunBank.Services;
 
-public class AccountService(DataAccountService dataAccountService, IMapper mapper)
+public class AccountService(DataAccountService dataAccountService, IMapper mapper) : IAccountService
 {
     public async Task<AccountViewModel?> GetAccountViewModelAsync(int id)
     {
