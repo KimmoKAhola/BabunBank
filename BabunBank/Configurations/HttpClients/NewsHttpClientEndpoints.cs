a6ca0c331962ee74a277e9e97d27be2ac65514c0
kahol <kahola@gmail.com>
2024-04-29 12:18:44 +0200

    Add NewsHttpClientEndpoints configuration
    A new configuration file named NewsHttpClientEndpoints has been added to the Configurations/HttpClients directory. This file defines several HTTP endpoints, including Login, Ad, Ads, Update, and Delete.

---
+++ BabunBank/Configurations/HttpClients/NewsHttpClientEndpoints.cs
@@ -0,0 +1,10 @@
+﻿namespace BabunBank.Configurations.HttpClients;
+
+public record NewsHttpClientEndPoints
+{
+    public string Login { get; init; } = null!;
+    public string Ad { get; init; } = null!;
+    public string Ads { get; init; } = null!;
+    public string Update { get; init; } = null!;
+    public string Delete { get; init; } = null!;
+}
