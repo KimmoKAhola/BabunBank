22627297d18d9d71deec8ba06990f19a81f60364
kahol <kahola@gmail.com>
2024-04-29 12:54:51 +0200

    Refactor ITransferService to ITransactionService
    ITransferService has been removed and replaced with ITransactionService in a new directory. The method signatures have been modified to accept Transaction and CreateTransferModel objects instead of individual parameters, improving readability and ease of use.

---
+++ BabunBank/Configurations/Interfaces/ITransactionService.cs
@@ -0,0 +1,11 @@
+﻿using BabunBank.Models.FormModels.TransferModels;
+using DataAccessLibrary.Data;
+
+namespace BabunBank.Configurations.Interfaces;
+
+public interface ITransactionService
+{
+    Task<bool?> CreateWithdrawalAsync(Transaction transaction);
+    Task<bool?> CreateDepositAsync(Transaction transaction);
+    Task<bool?> CreateTransferAsync(CreateTransferModel transfer);
+}
