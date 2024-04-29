b9018cdd0cefb376103e3fd6c3c49ab0e1b27ef3
kahol <kahola@gmail.com>
2024-04-29 12:55:23 +0200

    Add CustomerDropdownMenu enum
    A new enum, CustomerDropdownMenu, has been added under BabunBank/Configurations/Enums. The enum values represent different drop-down menu choices for a customer, including values varying from Twenty to Hundred.

---
+++ BabunBank/Configurations/Enums/CustomerDropdownMenu.cs
@@ -0,0 +1,10 @@
+﻿namespace BabunBank.Configurations.Enums;
+
+public enum CustomerDropdownMenu
+{
+    Twenty = 20,
+    TwentyFive = 25,
+    Fifty = 50,
+    SeventyFive = 75,
+    Hundred = 100
+}
