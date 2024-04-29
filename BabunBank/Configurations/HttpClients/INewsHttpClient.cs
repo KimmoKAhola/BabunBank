54776fe4efe8da7cdeabe1f17aff5dba544bf966
kahol <kahola@gmail.com>
2024-04-29 12:18:08 +0200

    Add INewsHttpClient interface
    A new interface INewsHttpClient has been introduced in the HttpClients directory under Configurations. This interface is designed to enable various HTTP client operations such as Get, Update, and Delete. It is a generic interface that takes a class type as a parameter.

---
+++ BabunBank/Configurations/HttpClients/INewsHttpClient.cs
@@ -0,0 +1,12 @@
+﻿using BabunBank.Models.FormModels.AdModels;
+
+namespace BabunBank.Configurations.HttpClients;
+
+public interface INewsHttpClient<T>
+    where T : class
+{
+    Task<T?> Get(int id);
+    Task<(IEnumerable<T>?, int?)> Get(int pageNumber, int pageSize);
+    Task<bool> Update(int id, EditAdModel model);
+    Task<bool> Delete(int id);
+}
