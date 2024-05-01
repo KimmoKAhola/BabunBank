0525ef90f0adf7b94135d7d912e3638d912c08b5
kahol <kahola@gmail.com>
2024-05-01 13:35:04 +0200

    Update MappingProfile with Account and Transaction mappings
    Two new mapping configurations are added to the MappingProfile class for Account to AccountViewModel and Transaction to TransactionViewModel conversion. This also involves summing transaction amounts for account balance and ordering transactions by TransactionId, plus mapping Disposition data to Account type and CustomerId.

--- Babun-API/Infrastructure/Configurations/MappingProfile.cs
+++ Babun-API/Infrastructure/Configurations/MappingProfile.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using BabunBank.Models.FormModels.Ad;
+using BabunBank.Models.ViewModels.Account;
+using BabunBank.Models.ViewModels.Transaction;
 using Babun_API.Models;
+using DataAccessLibrary.Data;
 
 namespace Babun_API.Infrastructure.Configurations;
 
@@ -17,5 +20,22 @@ public class MappingProfile : Profile
         CreateMap<Ads, ViewAdModel>().ReverseMap();
         CreateMap<Ads, CreateAdModel>().ReverseMap();
         CreateMap<EditAdModel, Ads>().ReverseMap();
+
+        CreateMap<Account, AccountViewModel>()
+            .ForMember(
+                dest => dest.Balance,
+                opt => opt.MapFrom(src => src.Transactions.Sum(t => t.Amount))
+            )
+            .ForMember(
+                dest => dest.Transactions,
+                opt => opt.MapFrom(src => src.Transactions.OrderByDescending(t => t.TransactionId))
+            )
+            .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Dispositions.First().Type))
+            .ForMember(
+                dest => dest.CustomerId,
+                opt => opt.MapFrom(src => src.Dispositions.First().CustomerId)
+            );
+
+        CreateMap<Transaction, TransactionViewModel>();
     }
 }
