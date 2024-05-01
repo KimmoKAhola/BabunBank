088a9eea5b712da0ae96544baa7fd719edef7a89
kahol <kahola@gmail.com>
2024-05-01 13:35:48 +0200

    Add account transaction retrieval endpoint
    This commit adds a new endpoint to the 'AccountController' that retrieves transactions for a given account ID with specified limit, offset and sort order. The new endpoint validates the input parameters and handles potential exceptions, ensuring a safe and precise retrieval of data.

--- Babun-API/Controllers/AccountController.cs
+++ Babun-API/Controllers/AccountController.cs
@@ -46,4 +46,50 @@ public class AccountController(DataAccountService dataAccountService, IMapper ma
             );
         }
     }
+
+    /// <summary>
+    /// Retrieves account transactions by account ID, limit, offset, and sort order.
+    /// </summary>
+    /// <param name="id">The ID of the account to retrieve transactions for.</param>
+    /// <param name="limit">The maximum number of transactions to retrieve.</param>
+    /// <param name="offset">The offset used for pagination.</param>
+    /// <param name="desc">The sorting order. Default value of true.</param>
+    /// <returns>A list of transactions for the specified account ID, limit, and offset.</returns>
+    [ProducesResponseType(typeof(TransactionViewModel), 200)]
+    [ProducesResponseType(400)]
+    [HttpGet("/account/{id:int}/{limit:int}/{offset:int}")]
+    public async Task<IActionResult> Account(int id, int limit, int offset, bool? desc = true)
+    {
+        if (id <= 0 || limit <= 0 || offset < 0)
+        {
+            return BadRequest("Wrong parameters provided");
+        }
+        try
+        {
+            var account = await dataAccountService.GetAsync(id);
+            if (account == null)
+            {
+                return BadRequest("Wrong id provided");
+            }
+            var result =
+                desc.HasValue && desc.Value
+                    ? account
+                        .Transactions.OrderByDescending(x => x.TransactionId)
+                        .Skip(offset)
+                        .Take(limit)
+                        .ToList()
+                    : account
+                        .Transactions.OrderBy(x => x.TransactionId)
+                        .Skip(offset)
+                        .Take(limit)
+                        .ToList();
+            var accountViewModel = mapper.Map<List<TransactionViewModel>>(result);
+
+            return Ok(accountViewModel);
+        }
+        catch (Exception e)
+        {
+            return BadRequest("Wrong parameters provided");
+        }
+    }
 }
