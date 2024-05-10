d11804acf7f000cd800495ed084a178d67ae64c4
kahol <kahola@gmail.com>
2024-05-10 16:51:22 +0200

    Update AssemblyInfo and remove Admin Details
    The AssemblyInfo for both BabunBank and Babun-API projects has been updated with a new version number. Additionally, the Admin Details page and its associated details from the EditorConfig file have been removed.

--- Babun-API/obj/Debug/net8.0/Babun-API.AssemblyInfo.cs
+++ Babun-API/obj/Debug/net8.0/Babun-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Babun-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca0701e93d85176798b03af64f9b939daf7a9443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ffc9037d057d6a3156fc1b515963c670b05df0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Babun-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Babun-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
