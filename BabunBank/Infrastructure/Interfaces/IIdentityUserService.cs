b9cb3ef4a23dd43daf6949af7cc3a8057905222d
kahol <kahola@gmail.com>
2024-05-10 16:45:02 +0200

    Update DeleteAsync method's return type to Task<bool>
    The return type of the DeleteAsync method in IIdentityUserService, DataIdentityUserService, and IdentityUserService has been changed from Task<bool?> to Task<bool>. This update ensures returned values will be either true or false, eliminating the possibility of null.

--- BabunBank/Infrastructure/Interfaces/IIdentityUserService.cs
+++ BabunBank/Infrastructure/Interfaces/IIdentityUserService.cs
@@ -8,6 +8,6 @@ public interface IIdentityUserService
     Task<IdentityUserViewModel> GetSingleAsync(string id);
     Task<bool> CreateAsync(IdentityUser model); //TODO delete?
     Task<IEnumerable<IdentityUserViewModel>> GetAllAsync();
-    Task<bool?> DeleteAsync(string id);
+    Task<bool> DeleteAsync(string id);
     Task<bool> CheckIfExistsByEmailAsync(string email);
 }
