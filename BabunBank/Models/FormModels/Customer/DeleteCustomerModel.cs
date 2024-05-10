279abe86944da78a22be0e5d9c3e63a67c03d67c
kahol <kahola@gmail.com>
2024-05-10 16:41:34 +0200

    Add model for customer deletion
    This commit introduces a new model named 'DeleteCustomerModel' in the FormModels/Customer directory. This model will be used when customers want to delete their accounts. It requires customers to provide their first name, surname, and Id, and confirm these details to prevent accidental deletions.

---
+++ BabunBank/Models/FormModels/Customer/DeleteCustomerModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BabunBank.Infrastructure.Configurations.CustomAnnotations;
+
+namespace BabunBank.Models.FormModels.Customer;
+
+public record DeleteCustomerModel
+{
+    [Required]
+    public string FirstName { get; init; } = null!;
+
+    [Required]
+    public string Surname { get; init; } = null!;
+
+    [Required]
+    public int Id { get; init; }
+
+    [Compare("Id", ErrorMessage = "Id has to match")]
+    public int ConfirmId { get; init; }
+
+    [CompareFullName(ErrorMessage = "Name should match the customer's name.")]
+    public string ConfirmFullName { get; init; } = null!;
+}
