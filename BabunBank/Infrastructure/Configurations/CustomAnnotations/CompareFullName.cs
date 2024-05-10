fc82bdfc7a15680d5e712fda206016cd1f1c886d
kahol <kahola@gmail.com>
2024-05-10 16:41:13 +0200

    Add CompareFullName custom validation attribute
    This commit introduces a new custom validation attribute named CompareFullName. This attribute is implemented in the BabunBank.Infrastructure.Configurations.CustomAnnotations namespace to validate the full name format for customer forms in the BabunBank application.

---
+++ BabunBank/Infrastructure/Configurations/CustomAnnotations/CompareFullName.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BabunBank.Models.FormModels.Customer;
+
+namespace BabunBank.Infrastructure.Configurations.CustomAnnotations;
+
+//TODO fix null checks
+public class CompareFullName : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var model = (DeleteCustomerModel)validationContext.ObjectInstance;
+        var fullName = model.FirstName + " " + model.Surname;
+
+        if (fullName != (string)value)
+        {
+            return new ValidationResult($"Name does not match the full name format: {fullName}");
+        }
+
+        return ValidationResult.Success;
+    }
+}
