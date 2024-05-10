53e578ca4c88498ec1d4b60d5015734b7c9094e4
kahol <kahola@gmail.com>
2024-05-10 16:41:46 +0200

    Add DeleteIdentityUserModel
    A new form model, DeleteIdentityUserModel, has been created for managing user deletion. It includes validation to ensure the same username is entered twice before proceeding with the deletion.

---
+++ BabunBank/Models/FormModels/IdentityUser/DeleteIdentityUserModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabunBank.Models.FormModels.IdentityUser;
+
+public record DeleteIdentityUserModel
+{
+    [Required]
+    public string UserId { get; init; } = null!;
+    public string Username { get; init; } = null!;
+
+    [Compare("Username", ErrorMessage = "Username has to match")]
+    public string ConfirmUsername { get; init; } = null!;
+}
