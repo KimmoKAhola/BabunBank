1cb5a2ebbcf1f896155faf5ac4a252ca6e5a6e2d
kahol <kahola@gmail.com>
2024-05-13 15:54:35 +0200

    Update Assembly Informational Version and sourcelink.json files
    The informational version attribute in AssemblyInfo.cs for Babun-API, DetectMoneyLaundering, DataAccessLibrary, and BabunBank has been updated. Similarly, the sourcelink.json files for the same libraries and applications have also been updated to reflect the recent changes.

--- DetectMoneyLaundering/obj/Debug/net8.0/DetectMoneyLaundering.AssemblyInfo.cs
+++ DetectMoneyLaundering/obj/Debug/net8.0/DetectMoneyLaundering.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d11804acf7f000cd800495ed084a178d67ae64c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8e79071035d7d090a2e52b128082d029b71394")]
 [assembly: System.Reflection.AssemblyProductAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
