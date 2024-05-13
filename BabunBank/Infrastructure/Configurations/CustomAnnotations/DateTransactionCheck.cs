86524a5b345ba1e77a466d00f51ca92feffebf35
kahol <kahola@gmail.com>
2024-05-13 13:45:49 +0200

    Add DateTransactionCheck custom validation attribute
    This commit introduces a new custom validation attribute named DateTransactionCheck. This attribute ensures that a transaction date is valid and not in the past. It's been added to the Infrastructure > Configurations > CustomAnnotations directory.

---
+++ BabunBank/Infrastructure/Configurations/CustomAnnotations/DateTransactionCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabunBank.Infrastructure.Configurations.CustomAnnotations;
+
+public class DateTransactionCheck : ValidationAttribute
+{
+    private static DateOnly Today => DateOnly.FromDateTime(DateTime.Now);
+
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not DateOnly)
+        {
+            return new ValidationResult("Please enter a valid date format");
+        }
+
+        if (value is DateOnly dateOnly && dateOnly >= Today)
+        {
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult("You can not enter a transaction date in the past.");
+    }
+}
