36214ee08d378be4cab1daa35eab43bd685c30bc
kahol <kahola@gmail.com>
2024-05-13 13:45:28 +0200

    Added UpdateIdentityUserModel
    This commit adds the UpdateIdentityUserModel under the FormModels namespace. The model includes required fields such as UserId and Email, with corresponding confirmation fields, and field validators. It also introduces a UserRole field to store the user's role level.

---
+++ BabunBank/Models/FormModels/IdentityUser/UpdateIdentityUserModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DataAccessLibrary.Data;
+
+namespace BabunBank.Models.FormModels.IdentityUser;
+
+public record UpdateIdentityUserModel
+{
+    [Required]
+    public string UserId { get; init; } = null!;
+
+    [Required]
+    public string Email { get; init; } = null!;
+
+    [EmailAddress]
+    [Compare("Email", ErrorMessage = "Email addresses has to match")]
+    public string ConfirmEmail { get; init; } = null!;
+
+    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$")]
+    public string Password { get; init; } = null!;
+
+    [Compare("Password", ErrorMessage = "Passwords has to match")]
+    public string ConfirmPassword { get; init; } = null!;
+
+    public bool EmailConfirmed { get; init; } = true;
+
+    public UserRole UserRole { get; init; }
+}
