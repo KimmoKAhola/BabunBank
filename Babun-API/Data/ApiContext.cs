ca068975fdb84b71f02d5d70d3ada8e0afc5228c
kahol <kahola@gmail.com>
2024-05-13 20:22:26 +0200

    Add comments to ApiContext class
    The commit adds a summary to the ApiContext class and to its constructor and property. The changes aim to make the code more understandable and organized, ensuring that each element of the code is well documented for better understanding of its purpose.

--- Babun-API/Data/ApiContext.cs
+++ Babun-API/Data/ApiContext.cs
@@ -3,12 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Babun_API.Data;
 
+/// <summary>
+/// Represents the context for the API.
+/// </summary>
 public class ApiContext : DbContext
 {
+    /// <inheritdoc />
     protected ApiContext() { }
 
+    /// <inheritdoc />
     public ApiContext(DbContextOptions<ApiContext> options)
         : base(options) { }
 
+    /// <summary>
+    /// Represents an advertisement.
+    /// </summary>
     public DbSet<Ads> Ads { get; init; }
 }
