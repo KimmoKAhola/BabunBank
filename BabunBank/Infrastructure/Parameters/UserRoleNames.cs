ca813406318c18561f5afdd0d2e377a71e8f29bd
kahol <kahola@gmail.com>
2024-05-13 20:15:47 +0200

    Remove Customer role from UserRoleNames
    The 'Customer' role has been removed from UserRoleNames.cs, as per the existing to-do comment. This change also implies that the customer seeding will no longer be necessary and has been removed.

--- BabunBank/Infrastructure/Parameters/UserRoleNames.cs
+++ BabunBank/Infrastructure/Parameters/UserRoleNames.cs
@@ -6,5 +6,4 @@ public static class UserRoleNames
 {
     public const string Admin = nameof(UserRole.Admin);
     public const string Cashier = nameof(UserRole.Cashier);
-    public const string Customer = nameof(UserRole.Customer); //TODO remove this and remove the customer seeding.
 }
