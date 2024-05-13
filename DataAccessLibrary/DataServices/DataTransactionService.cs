eccc80a7bd475127a89bd5c8cba114d63a2df9f8
kahol <kahola@gmail.com>
2024-05-13 20:19:49 +0200

    Remove redundant comments in DataTransactionService
    Removed unnecessary comments from the CreateDepositAsync and CreateWithdrawAsync methods in DataTransactionService. The code is self-explanatory, so the comments were not needed for understanding. This change makes the code cleaner and increases readability.

--- DataAccessLibrary/DataServices/DataTransactionService.cs
+++ DataAccessLibrary/DataServices/DataTransactionService.cs
@@ -11,27 +11,22 @@ public class DataTransactionService(
 {
     public async Task<bool?> CreateDepositAsync(Transaction deposit)
     {
-        //Get the account associated with the model
         var account = await accountRepository.GetAsync(x => x.AccountId == deposit.AccountId);
         if (account == null)
             return null!;
 
-        //Create a new transaction
         var newTransaction = await transactionRepository.CreateAsync(deposit);
 
         if (newTransaction == null)
             return false;
 
-        //Sum of all transaction amounts
         var newAccountBalance = await transactionRepository
             .GetAllAsync()
             .Where(x => x.AccountId == newTransaction.AccountId)
             .SumAsync(x => x.Amount);
 
-        //Update the balance according to transactions
         account.Balance = newAccountBalance;
 
-        //Update the account. Check for null here? TODO
         await accountRepository.UpdateAsync(x => x.AccountId == account.AccountId, account);
         return true;
     }
@@ -42,22 +37,18 @@ public class DataTransactionService(
         if (account == null)
             return null!;
 
-        //Create a new transaction
         var newTransaction = await transactionRepository.CreateAsync(withdrawal);
 
         if (newTransaction == null)
             return false;
 
-        //Sum of all transaction amounts
         var newAccountBalance = await transactionRepository
             .GetAllAsync()
             .Where(x => x.AccountId == newTransaction.AccountId)
             .SumAsync(x => x.Amount);
 
-        //Update the balance according to transactions
         account.Balance = newAccountBalance;
 
-        //Update the account. Check for null here? TODO
         await accountRepository.UpdateAsync(x => x.AccountId == account.AccountId, account);
         return true;
     }
