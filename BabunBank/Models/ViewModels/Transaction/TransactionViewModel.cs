2d42aecb2c91634b6b8af3fe9880f8654d7bdfa8
kahol <kahola@gmail.com>
2024-05-13 19:45:11 +0200

    Uncomment AccountId in TransactionViewModel
    Reinstated the AccountId property in the TransactionViewModel class that had been previously commented out. This change enables the correct relationship between transactions and account objects.

--- BabunBank/Models/ViewModels/Transaction/TransactionViewModel.cs
+++ BabunBank/Models/ViewModels/Transaction/TransactionViewModel.cs
@@ -4,7 +4,7 @@ public class TransactionViewModel
 {
     public int TransactionId { get; set; }
 
-    // public int AccountId { get; set; }
+    public int AccountId { get; set; }
     public DateOnly Date { get; set; }
     public string? Type { get; set; }
     public string? Operation { get; set; }
