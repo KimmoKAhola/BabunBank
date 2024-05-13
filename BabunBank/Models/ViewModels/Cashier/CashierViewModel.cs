bf261d9ee36d18cfbc2d74e6bfc3a51ec7b1089b
kahol <kahola@gmail.com>
2024-05-13 20:20:01 +0200

    Removed _SecondaryLayout view and CashierViewModel model
    The _SecondaryLayout.cshtml view and CashierViewModel.cs from the BabunBank project were deleted. These resources seem to be unused or redundant, and thus have been removed for better code maintenance and clarity.

--- BabunBank/Models/ViewModels/Cashier/CashierViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using BabunBank.Models.ViewModels.Customer;
-
-namespace BabunBank.Models.ViewModels.Cashier;
-
-public class CashierViewModel
-{
-    public IEnumerable<CustomerViewModel> CustomerViewModels { get; set; }
-    // public
-}
