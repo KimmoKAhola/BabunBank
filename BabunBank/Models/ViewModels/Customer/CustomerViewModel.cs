e0e59f4538c73b2f80c2fb2dd4add834a1fae6ae
kahol <kahola@gmail.com>
2024-05-13 20:20:56 +0200

    Refactor CustomerViewModel to a record type
    The CustomerViewModel has been converted into a record type for better immutability control. The comments and an unnecessary empty line in the file have been cleared for code cleanliness. The IsDeleted property is confirmed to be used and the TODO comment is removed.

--- BabunBank/Models/ViewModels/Customer/CustomerViewModel.cs
+++ BabunBank/Models/ViewModels/Customer/CustomerViewModel.cs
@@ -2,17 +2,7 @@
 
 namespace BabunBank.Models.ViewModels.Customer;
 
-/// <summary>
-/// Det ska gå att söka efter kund på namn och stad
-/// En lista ska visas med kundnummer, personnummer, namn, adress, stad
-/// Paginerat med 50* (dropdown med olika val)
-///
-/// Det ska gå att se kundens totala saldo för alla konton.
-///
-/// Lägg till en kontobild där man kan se transaktioner för ett individuellt konto.
-/// Kontosida som visar kontonummer och saldo samt en lista med transaktioner i desc order.
-/// </summary>
-public class CustomerViewModel
+public record CustomerViewModel
 {
     public int CustomerId { get; init; }
     public string? Gender { get; init; }
@@ -22,10 +12,8 @@ public class CustomerViewModel
     public string? City { get; init; }
     public string? Zipcode { get; init; }
     public string? Country { get; init; }
-
     public DateOnly? BirthDay { get; init; }
     public string? NationalId { get; init; }
-
-    public bool IsDeleted { get; init; } //TODO is this needed? Only used for soft deletion
+    public bool IsDeleted { get; init; }
     public List<AccountViewModel>? CustomerAccounts { get; init; }
 }
