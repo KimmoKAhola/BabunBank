75bba5c35c9cb7140c1690738abc89c93ee64bad
kahol <kahola@gmail.com>
2024-05-16 16:24:40 +0200

    Refactor main program to use MenuService
    Removed direct utilization of MoneyLaunderingService and migrated the responsibility of displaying data to a newly added MenuService in the main program. This enhances modularity, thus making the code easier to manage and test.

--- DetectMoneyLaundering/Program.cs
+++ DetectMoneyLaundering/Program.cs
@@ -1,11 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using DataAccessLibrary.Data;
+﻿using DataAccessLibrary.Data;
 using DataAccessLibrary.DataServices;
 using DataAccessLibrary.Repositories;
 using DetectMoneyLaundering;
 using DetectMoneyLaundering.Interfaces;
-using DetectMoneyLaundering.Models;
 using DetectMoneyLaundering.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,25 +15,11 @@ builder.ConfigureServices(services =>
     services.AddScoped<AccountRepository>();
     services.AddScoped<DataAccountService>();
     services.AddScoped<IMoneyLaunderingService, MoneyLaunderingService>();
+    services.AddScoped<IMenuService, MenuService>();
 });
 
 var serviceProvider = builder.Build().Services;
 
-var moneyLaunderingService = serviceProvider.GetRequiredService<MoneyLaunderingService>();
+var menu = serviceProvider.GetRequiredService<IMenuService>();
 
-var model = await moneyLaunderingService.InspectAccount(2, VisualizationModes.Console);
-var list = await moneyLaunderingService.InspectAllAccounts();
-var scalingModel = new PlotScalingModel
-{
-    HeightScaleFactor = 4.0,
-    WidthScaleFactor = 4.0,
-    FontScaleFactor = 10
-};
-
-DataVisualizationService.CreateIndividualPlot(model, VisualizationModes.Console, scalingModel);
-DataVisualizationService.CreatePlotForAllTransactions(
-    list,
-    VisualizationModes.Console,
-    scalingModel
-);
-DataVisualizationService.CreateHistogram(list, VisualizationModes.Console);
+await menu.Display();
