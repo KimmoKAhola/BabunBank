1fa309836eba475410e4b410204e300f7029c71f
kahol <kahola@gmail.com>
2024-05-16 14:08:55 +0200

    Update IIdentityUserService interface
    Added a new method for updating user password and changed the return type for the UpdateAsync method in the IIdentityUserService interface. The UpdateAsync method now returns an IdentityResult instead of a boolean. Also, imported the Microsoft.AspNetCore.Identity namespace.

--- BabunBank/Infrastructure/Interfaces/IIdentityUserService.cs
+++ BabunBank/Infrastructure/Interfaces/IIdentityUserService.cs
@@ -1,5 +1,6 @@
 ﻿using BabunBank.Models.FormModels.IdentityUser;
 using BabunBank.Models.ViewModels.Admin;
+using Microsoft.AspNetCore.Identity;
 using IdentityUser = Microsoft.AspNetCore.Identity.IdentityUser;
 
 namespace BabunBank.Infrastructure.Interfaces;
@@ -8,8 +9,9 @@ public interface IIdentityUserService
 {
     Task<IdentityUserViewModel> GetSingleAsync(string id);
     Task<bool> CreateAsync(IdentityUser model); //TODO delete?
-    Task<bool> UpdateAsync(UpdateIdentityUserModel model);
+    Task<IdentityResult> UpdateAsync(UpdateIdentityUserModel model);
     Task<IEnumerable<IdentityUserViewModel>> GetAllAsync();
     Task<bool> DeleteAsync(string id);
     Task<bool> CheckIfExistsByEmailAsync(string email);
+    Task<IdentityResult> UpdatePasswordAsync(string id, UpdateIdentityUserPasswordModel model);
 }
