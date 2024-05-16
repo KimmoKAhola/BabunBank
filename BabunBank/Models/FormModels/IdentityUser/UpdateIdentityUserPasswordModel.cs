9dd920c08872f037116d19b22987275544a306b1
kahol <kahola@gmail.com>
2024-05-16 14:07:47 +0200

    Add 'UpdateIdentityUserPasswordModel' class
    A new model class 'UpdateIdentityUserPasswordModel' was added to handle password updates of identity users. This class includes username, user id, and password-related properties with required validations. It primarily aims to ensure the correctness and match of new and old passwords, enhancing security and user management functionalities.

---
+++ BabunBank/Models/FormModels/IdentityUser/UpdateIdentityUserPasswordModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabunBank.Models.FormModels.IdentityUser;
+
+public record UpdateIdentityUserPasswordModel
+{
+    public string Username { get; init; } = null!;
+    public string UserId { get; init; } = null!;
+
+    [Required]
+    public string OldPassword { get; init; } = null!;
+
+    [Compare("OldPassword", ErrorMessage = "Passwords has to match")]
+    public string ConfirmOldPassword { get; init; } = null!;
+
+    [Required]
+    public string NewPassword { get; init; } = null!;
+
+    [Required]
+    [Compare("NewPassword", ErrorMessage = "Passwords has to match")]
+    public string ConfirmNewPassword { get; init; } = null!;
+}
