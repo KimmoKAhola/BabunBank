8ac258558d0e4ce1fabf673c30c23548eeec79c8
kahol <kahola@gmail.com>
2024-05-16 14:05:45 +0200

    Refactor AccountFactory and relocate file
    The AccountFactory file has been moved to an Account specific folder under Factories. The file has also been modified to use a constant for the initial balance value rather than a hardcoded value, improving code maintainability. Additionally, the create method now creates an instance of DataAccessLibrary.Data.Account, improving code clarity.

---
+++ BabunBank/Factories/Account/AccountFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace BabunBank.Factories.Account;
+
+public static class AccountFactory
+{
+    private const decimal InitialBalance = 0m;
+
+    public static DataAccessLibrary.Data.Account Create()
+    {
+        return new DataAccessLibrary.Data.Account
+        {
+            Frequency = "AfterTransaction", //TODO add as parameter
+            Created = DateOnly.FromDateTime(DateTime.Now),
+            Balance = InitialBalance
+        };
+    }
+}
