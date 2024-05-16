53b695e7cd68ddc0e2d31cd1cacc21be9f340b2e
kahol <kahola@gmail.com>
2024-05-16 16:24:53 +0200

    Add DispositionOwner constant to Parameters class
    A new constant, DispositionOwner, has been added to the Parameters class. This constant will help in identifying and processing the user's dispositional status in transactions within the DetectMoneyLaundering project.

--- DetectMoneyLaundering/Infrastructure/Parameters.cs
+++ DetectMoneyLaundering/Infrastructure/Parameters.cs
@@ -5,4 +5,5 @@ public static class Parameters
     public const int ScalingDefault = 10;
     public const int ScalingDivider = 10;
     public const decimal TransferLimit = 15_000m;
+    public const string DispositionOwner = "owner";
 }
