284b3b8cafdd8b76563cfe88afd1ec3e8698e0a1
kahol <kahola@gmail.com>
2024-05-16 14:08:10 +0200

    Simplify ErrorController, remove NotFound404
    This commit simplifies the ErrorController by removing the NotFound404 method. It also cleans up unused "Microsoft.AspNetCore.Diagnostics" namespace. This reduces overall complexity and aligns the controller with the currently needed functionality.

--- BabunBank/Controllers/ErrorController.cs
+++ BabunBank/Controllers/ErrorController.cs
@@ -1,19 +1,11 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace BabunBank.Controllers;
 
 public class ErrorController : Controller
 {
-    // GET
     public IActionResult Index()
     {
-        return View();
-    }
-
-    public IActionResult NotFound404()
-    {
-        Response.StatusCode = 404;
         return View();
     }
 }
