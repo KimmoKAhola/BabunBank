c85b273e04db2c9ba7469f6b1cbf6bbdb16df3e1
kahol <kahola@gmail.com>
2024-05-16 14:11:09 +0200

    Refactor_UpdateIdentityUserModel_for_email_update
    Refactored the UpdateIdentityUserModel class for handling email updates. Removed password related attributes and replaced them with new and old email fields for enabling email change functionality. The validation fields for the new email addresses have also been added.

--- BabunBank/Models/FormModels/IdentityUser/UpdateIdentityUserModel.cs
+++ BabunBank/Models/FormModels/IdentityUser/UpdateIdentityUserModel.cs
@@ -9,19 +9,17 @@ public record UpdateIdentityUserModel
     public string UserId { get; init; } = null!;
 
     [Required]
-    public string Email { get; init; } = null!;
+    public string OldEmail { get; init; } = null!;
 
     [EmailAddress]
-    [Compare("Email", ErrorMessage = "Email addresses has to match")]
-    public string ConfirmEmail { get; init; } = null!;
+    [Compare("OldEmail", ErrorMessage = "Email addresses has to match")]
+    public string ConfirmOldEmail { get; init; } = null!;
 
-    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$")]
-    public string Password { get; init; } = null!;
+    [Required]
+    public string NewEmail { get; init; } = null!;
 
-    [Compare("Password", ErrorMessage = "Passwords has to match")]
-    public string ConfirmPassword { get; init; } = null!;
-
-    public bool EmailConfirmed { get; init; } = true;
-
+    [EmailAddress]
+    [Compare("NewEmail", ErrorMessage = "Email addresses has to match")]
+    public string ConfirmNewEmail { get; init; } = null!;
     public UserRole UserRole { get; init; }
 }
