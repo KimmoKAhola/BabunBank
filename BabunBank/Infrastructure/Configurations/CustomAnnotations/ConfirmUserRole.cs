cc358e34d018b6fb2537fd041edfcdfe74761b10
kahol <kahola@gmail.com>
2024-05-19 17:24:29 +0200

    Add custom validation attribute for user role
    This commit introduces a new custom validation attribute, ConfirmUserRole, in the BabunBank infrastructure configurations. This attribute validates if a valid user role has been chosen, enhancing data validation and integrity in the application.

---
+++ BabunBank/Infrastructure/Configurations/CustomAnnotations/ConfirmUserRole.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DataAccessLibrary.Data;
+
+namespace BabunBank.Infrastructure.Configurations.CustomAnnotations;
+
+public class ConfirmUserRole : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is > UserRole.Choose)
+        {
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult("Please choose a valid option");
+    }
+}
