51fe1d5e71fce5fde2a3e3669fc3926bb112c2c5
kahol <kahola@gmail.com>
2024-05-19 17:28:31 +0200

    Add GetTransferAccountViewModelAsync method to IAccountService interface
    The interface IAccountService has been updated to include the GetTransferAccountViewModelAsync method. Now there is a method that fetches the ViewModel specifically for Transfer operations.

--- BabunBank/Infrastructure/Interfaces/IAccountService.cs
+++ BabunBank/Infrastructure/Interfaces/IAccountService.cs
@@ -6,6 +6,7 @@ namespace BabunBank.Infrastructure.Interfaces;
 public interface IAccountService
 {
     Task<AccountViewModel?> GetAccountViewModelAsync(int id);
+    Task<AccountViewModel?> GetTransferAccountViewModelAsync(int id);
     Task<(IEnumerable<Account>, int NumberOfAccounts)> RenameMe(
         int id,
         int pageNumber,
