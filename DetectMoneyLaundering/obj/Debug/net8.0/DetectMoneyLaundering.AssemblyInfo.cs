c359cc3985bcfdcf0302a74e12182eed5f1b3dad
kahol <kahola@gmail.com>
2024-05-20 18:10:39 +0200

    Update footer details and assembly version
    The contact details in the site footer have been updated to include full address and correct name. Meanwhile, the assembly version of DetectMoneyLaundering project is also updated as indicated in AssemblyInformationalVersion attribute.

--- DetectMoneyLaundering/obj/Debug/net8.0/DetectMoneyLaundering.AssemblyInfo.cs
+++ DetectMoneyLaundering/obj/Debug/net8.0/DetectMoneyLaundering.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+750d8d59b9da8ff97396f628905e5c0b19ab34b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e999916b423b8450aba59e834b8f89d3f97da4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DetectMoneyLaundering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
