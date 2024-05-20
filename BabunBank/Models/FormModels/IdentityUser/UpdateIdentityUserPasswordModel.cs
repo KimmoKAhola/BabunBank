1b613599f0756b732c5779ca852db4e205452ccc
kahol <kahola@gmail.com>
2024-05-20 17:05:53 +0200

    Add custom error messages to password fields
    This commit modifies the 'UpdateIdentityUserPasswordModel' class by adding custom error messages to the 'OldPassword' and 'NewPassword' required fields. This provides clearer feedback to the user when they leave these fields empty.

--- BabunBank/Models/FormModels/IdentityUser/UpdateIdentityUserPasswordModel.cs
+++ BabunBank/Models/FormModels/IdentityUser/UpdateIdentityUserPasswordModel.cs
@@ -7,13 +7,13 @@ public record UpdateIdentityUserPasswordModel
     public string Username { get; init; } = null!;
     public string UserId { get; init; } = null!;
 
-    [Required]
+    [Required(ErrorMessage = "This field is required.")]
     public string OldPassword { get; init; } = null!;
 
     [Compare("OldPassword", ErrorMessage = "Passwords has to match")]
     public string ConfirmOldPassword { get; init; } = null!;
 
-    [Required]
+    [Required(ErrorMessage = "This field is required.")]
     public string NewPassword { get; init; } = null!;
 
     [Required]
